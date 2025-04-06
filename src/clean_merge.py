d30edc0bcaca7ec8edce967078f7266ecb976281
AntoineHocquet <antoine.hocquet86@gmail.com>
2025-04-06 17:53:54 +0200

    polished all including booster_version column

--- src/clean_merge.py
+++ src/clean_merge.py
@@ -2,6 +2,7 @@
 
 import pandas as pd
 import os
+import re
 from src.utils.config_loader import load_config
 
 def load_data(api_path: str, web_path: str):
@@ -10,6 +11,11 @@ def load_data(api_path: str, web_path: str):
     print(f"Loaded API data: {df_api.shape} rows")
     print(f"Loaded Web data: {df_web.shape} rows")
     return df_api, df_web
+
+
+def extract_date(date_series):
+    return date_series.dt.date
+
 
 
 def clean_api_data(df_api, remove_falcon_1=True):
@@ -21,20 +27,26 @@ def clean_api_data(df_api, remove_falcon_1=True):
     Output: same dataframe but with corrected types and removed falcon 1 flights by default
     """
     # Convert types
-    df_api['date'] = pd.to_datetime(df_api['date_utc'])
+    df_api['date'] = pd.to_datetime(df_api['date'])
     # df_api['outcome'] = df_api['outcome'].astype('Int64')  # Allows NaNs
 
-    # Removes Falcon 1 booster version to keep only Falcon 9
+    # Removes Falcon 1 boosters to keep only Falcon 9
     if remove_falcon_1:
         df_api=df_api[df_api['BoosterVersion']!='Falcon 1']
         print("Falcon 1 entries have been removed.")
-    
+
     # Calculate the mean of PayloadMass column to replace the np.nan values
     mean_payload = df_api['payload_mass'].mean()
     df_api['payload_mass'].fillna(mean_payload, inplace=True)
     print("Missing Payload Mass entries have been replaced by mean value.")
 
     return df_api
+
+
+def clean_Version_Booster(Version):
+    """ cleans the 'Version Booster' column"""
+    Version = Version.apply(lambda x: re.sub(r'[^\w\s\.]', '', str(x)))
+    return Version
 
 
 def clean_web_data(df_web, remove_falcon_1=True):
@@ -47,8 +59,9 @@ def clean_web_data(df_web, remove_falcon_1=True):
     # Drop unnamed columns or footnotes if any
     df_web = df_web.loc[:, ~df_web.columns.str.contains('^Unnamed')]
 
-    # Remove rows where there is no booster info
-    #df_web = df_web[df_web['Version Booster'].notna()]
+    # cleans the 'Version Booster' column
+    df_web['Version Booster'] = clean_Version_Booster(df_web['Version Booster'])
+    df_web['Version Booster'] = df_web['Version Booster'].str.strip()
 
     return df_web.copy()
 
@@ -128,11 +141,11 @@ def main():
         'Date': 'date',
         'Time': 'time',
     }
-    df_web_std = standardize_columns(df_web, web_column_map)
+    df_web_std = standardize_columns(df_web_clean, web_column_map)
 
     # Merging along flight_number & keep only the desired columns
     merged_df = pd.merge(
-        df_api_std[['flight_number', 'booster', 'payload_mass', 'outcome', 'date_utc', 'longitude', 'latitude', 'reused', 'reused_count', 'serial', 'landing_pad', 'gridfins', 'legs']],
+        df_api_std[['flight_number', 'booster', 'payload_mass', 'outcome', 'date', 'longitude', 'latitude', 'reused', 'reused_count', 'serial', 'landing_pad', 'gridfins', 'legs']],
         df_web_std[['flight_number', 'booster_version', 'orbit', 'launch_site']], # 'launch_outcome' not necessary
         on='flight_number',
         how='inner' # keeps only the launches that are present in both frames
@@ -147,6 +160,7 @@ def main():
     print(f"\nNew dataframe has {df_final.shape[0]} rows and {df_final.shape[1]} columns.")
     print(f"\nData successfully saved to {output_csv} for EDA & dashboard use.")
     print("\nOverview (first ten rows):\n", df_final.head(10))
+
 
 if __name__ == "__main__":
     main()
--- src/eda/visualizations.py
+++ src/eda/visualizations.py
@@ -25,23 +25,6 @@ def plot_landing_distribution(df):
     plt.close()
 
 
-# def plot_booster_versions(df):
-#     plt.figure(figsize=(12, 6))
-#     sns.countplot(data=df, x='booster_version', order=df['booster_version'].value_counts().index)
-#     plt.title("Booster Version Category Counts")
-#     plt.xticks(rotation=45)
-#     plt.tight_layout()
-#     plt.savefig(DOCS_PATH / "booster_version_counts.png")
-#     plt.close()
-
-
-# def plot_payload_vs_success(df):
-#     plt.figure(figsize=(10, 6))
-#     sns.scatterplot(data=df, x='payload_mass', y='class', hue='booster_version')
-#     plt.title("Payload vs. Success")
-#     plt.savefig(DOCS_PATH / "payload_vs_success.png")
-#     plt.close()
-
 def plot_payload_vs_success(df):
     plt.figure(figsize=(10, 6))
     sns.catplot(y='payload_mass', x='flight_number', hue="class", data=df, aspect = 5)
@@ -71,7 +54,7 @@ def plot_launch_site_vs_payload_mass(df):
 
 def plot_success_rate_vs_orbit(df):
     plt.figure(figsize=(10, 6))
-    df1=pd.DataFrame(df.groupby('Orbit')['Class'].mean())
+    df1=pd.DataFrame(df.groupby('orbit')['class'].mean())
     sns.barplot(data=df1,x='orbit',y='class',hue='class')
     plt.xlabel('Type of orbit')
     plt.ylabel('Success rate')
@@ -90,7 +73,7 @@ def plot_orbit_vs_flight_number(df):
 
 def plot_orbit_vs_payload_mass(df):
     plt.figure(figsize=(10, 6))
-    sns.catplot(x="PayloadMass", y="Orbit", hue="Class", data=df)
+    sns.catplot(x="payload_mass", y="orbit", hue="class", data=df)
     plt.xlabel("Pay load Mass (kg)")
     plt.ylabel("Orbit")
     plt.savefig(DOCS_PATH / "orbit_vs_payload_mass.png")
@@ -99,18 +82,19 @@ def plot_orbit_vs_payload_mass(df):
 
 def extract_year_from_date(df):
     """
-    A simple function to extract years from the date_utc column
+    A simple function to extract years from the 'date' column
     Args: df A dataframe with a date_utc column
     Returns: same dataframe with a year column
       """
-    df['year'] = df['date_utc'].str.slice(0, 4)
+    df['year'] = df['date'].str.slice(0, 4)
     return df
 
 
 # Plot a line chart with x axis to be the extracted year and y axis to be the success rate
 def plot_success_rate_vs_year(df):
+    df1=extract_year_from_date(df)
     plt.figure(figsize=(10, 6))
-    sns.lineplot(x="year", y="class", data=pd.DataFrame(df.groupby('year')['class'].mean()))
+    sns.lineplot(x="year", y="class", data=pd.DataFrame(df1.groupby('year')['class'].mean()))
     plt.xlabel("Year")
     plt.ylabel("Success rate")
     plt.savefig(DOCS_PATH / "success_rate_vs_year.png")
@@ -118,16 +102,19 @@ def plot_success_rate_vs_year(df):
 
 # ============================================================================
 def generate_all():
-    df = load_data()
+    df= load_data()
     plot_landing_distribution(df)
-    plot_booster_versions(df)
     plot_payload_vs_success(df)
+    plot_launch_site_vs_flight_number(df)
+    plot_launch_site_vs_payload_mass(df)
+    plot_success_rate_vs_orbit(df)
+    plot_orbit_vs_flight_number(df)
+    plot_orbit_vs_payload_mass(df)
+    plot_success_rate_vs_year(df)
     print(f"Plots saved to {DOCS_PATH.resolve()}")
 
 
 if __name__ == "__main__":
-    # generate_all()
-    df= pd.read_csv(DATA_PATH)
-    df1=extract_year_from_date(df)
-    print(df1.head())
-    plot_success_rate_vs_year(df1)
+    
+    generate_all()
+    
